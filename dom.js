a5b408544a2e90e5a783cf3d496abca5b54ece8a
revanthyadav13 <revanthyadav13@gmail.com>
2023-05-10 14:53:16 +0530

    document.getElementsByClassName

--- dom.js
+++ dom.js
@@ -1,2 +1,8 @@
-var headerTitle=document.getElementById('header-title')
+var headerTitle=document.getElementById('header-title');
 console.log(headerTitle);
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+items[1].textContent="Hello";
+items[1].style.fontWeight="bold";
+items[1].style.backgroundColor="yellow";
